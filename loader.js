22004b9786b610e1cc2384cde78a2aae8bd38ec8
Mark Vasilkov <mvasilkov@gmail.com>
2015-09-03 00:05:09 +0300

    Kokonotsuboshi Girls' Academy of Commerce

--- loader.js
+++ loader.js
@@ -1,45 +1,41 @@
-var xxxxxxxxxxxxxxxx = 'xxxxxxxxxxxxxxxx'
+var cellsize = 60
+var xxxxxxxxxxxxxxxx = ['xxxxxxxxxxxxxxxx']
 
 levels = levels.map(function (level) {
-    level.unshift(xxxxxxxxxxxxxxxx)
-    level.push(xxxxxxxxxxxxxxxx)
-    return loadLevel(level)
+    return loadLevel(xxxxxxxxxxxxxxxx.concat(level).concat(xxxxxxxxxxxxxxxx))
 })
 
-function loadLevel(level) {
+function loadLevel(ascii) {
     var lowerPlatforms = [], upperPlatforms = []
     var i, j
+
+    function canHazPlatform(platforms) {
+        if (platforms.kurwa) {
+            var b = platforms.pop()
+            platforms.push([i, j, b[2] + 1])
+        }
+        else {
+            platforms.push([i, j, 1])
+            platforms.kurwa = true
+        }
+    }
 
     for (j = 9; j--;) {
         lowerPlatforms.kurwa = false
         upperPlatforms.kurwa = false
 
         for (i = 16; i--;) {
-            if (level[j][i] == 'x') {
+            if (ascii[j][i] == 'x') {
                 /* lowerPlatforms */
-                if (j != 8 && level[j + 1][i] != 'x') {
-                    if (lowerPlatforms.kurwa) {
-                        var b = lowerPlatforms.pop()
-                        lowerPlatforms.push([i, j, b[2] + 1])
-                    }
-                    else {
-                        lowerPlatforms.push([i, j, 1])
-                        lowerPlatforms.kurwa = true
-                    }
+                if (j != 8 && ascii[j + 1][i] != 'x') {
+                    canHazPlatform(lowerPlatforms)
                 }
                 else {
                     lowerPlatforms.kurwa = false
                 }
                 /* upperPlatforms */
-                if (j != 0 && level[j - 1][i] != 'x') {
-                    if (upperPlatforms.kurwa) {
-                        var b = upperPlatforms.pop()
-                        upperPlatforms.push([i, j, b[2] + 1])
-                    }
-                    else {
-                        upperPlatforms.push([i, j, 1])
-                        upperPlatforms.kurwa = true
-                    }
+                if (j != 0 && ascii[j - 1][i] != 'x') {
+                    canHazPlatform(upperPlatforms)
                 }
                 else {
                     upperPlatforms.kurwa = false
@@ -51,6 +47,7 @@ function loadLevel(level) {
             }
         }
     }
+
 
     var platforms = []
 
@@ -66,15 +63,69 @@ function loadLevel(level) {
         return b.y - a.y
     })
 
-    return platforms
+
+    lowerPlatforms = [], upperPlatforms = []
+
+    for (i = 16; i--;) {
+        lowerPlatforms.kurwa = false
+        upperPlatforms.kurwa = false
+
+        for (j = 9; j--;) {
+            if (ascii[j][i] == 'x') {
+                /* lowerPlatforms */
+                if (i != 15 && ascii[j][i + 1] != 'x') {
+                    canHazPlatform(lowerPlatforms)
+                }
+                else {
+                    lowerPlatforms.kurwa = false
+                }
+                /* upperPlatforms */
+                if (i != 0 && ascii[j][i - 1] != 'x') {
+                    canHazPlatform(upperPlatforms)
+                }
+                else {
+                    upperPlatforms.kurwa = false
+                }
+            }
+            else {
+                lowerPlatforms.kurwa = false
+                upperPlatforms.kurwa = false
+            }
+        }
+    }
+
+
+    var walls = []
+
+    for (i = lowerPlatforms.length; i--;) {
+        walls.push(toCoordinates2(lowerPlatforms[i], 1))
+    }
+
+    for (i = upperPlatforms.length; i--;) {
+        walls.push(toCoordinates2(upperPlatforms[i]))
+    }
+
+    walls.sort(function (a, b) {
+        return b.y - a.y
+    })
+
+    return {a: ascii, p: platforms, w: walls}
 }
 
 function toCoordinates(b, inc) {
-    var cellsize = 60
-    var altitude = cellsize * (b[1] + (0|inc))
+    var a = cellsize * (b[1] + (0|inc))
 
     return [
-        {x: cellsize * b[0], y: altitude},
-        {x: cellsize * (b[0] + b[2]), y: altitude}
+        {x: cellsize * b[0], y: a},
+        {x: cellsize * (b[0] + b[2]), y: a}
+    ]
+}
+
+function toCoordinates2(b, inc) {
+    var a = cellsize * (b[0] + (0|inc))
+
+    return [
+        {x: a, y: cellsize * b[1]},
+        {x: a, y: cellsize * (b[1] + b[2])}
     ]
 }
--- paint.js
+++ paint.js
@@ -1,9 +1,26 @@
 var canvas = $c.getContext('2d')
 
 function paintLevel(level) {
+    var i, j
+
     canvas.beginPath()
 
-    level.forEach(function (platform) {
+    for (i = 16; i--;) {
+        for (j = 9; j--;) {
+            if (level.a[j][i] == 'x') {
+                canvas.rect(cellsize * i, cellsize * j, cellsize, cellsize)
+            }
+        }
+    }
+
+    canvas.fillStyle = '#586E75'
+    canvas.fill()
+
+
+    canvas.beginPath()
+
+    level.p.concat(level.w)
+    .forEach(function (platform) {
         canvas.moveTo(platform[0].x, platform[0].y)
         canvas.lineTo(platform[1].x, platform[1].y)
     })
