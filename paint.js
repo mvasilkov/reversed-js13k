e1d4bdc6db155267ea2f7cf5adc231d8fbb099db
Mark Vasilkov <markv@infinidat.com>
2015-09-03 14:55:33 +0300

    Pattern

--- paint.js
+++ paint.js
@@ -16,6 +16,9 @@ function paintLevel(level) {
     canvas.fillStyle = '#586E75'
     canvas.fill()
 
+    canvas.fillStyle = '#FFFBEB'
+    paintPattern()
+
 
     canvas.beginPath()
 
@@ -25,9 +28,40 @@ function paintLevel(level) {
         canvas.lineTo(platform[1].x, platform[1].y)
     })
 
+    canvas.lineCap = 'square'
     canvas.lineWidth = 2
     canvas.strokeStyle = '#EEFF41'
     canvas.stroke()
+}
+
+function paintPattern() {
+    var step = 20, pad = 10
+    var thickness = 2, inc = 0.000626
+    var a = Math.random() < 0.5? -0.1: 0.1
+    var i, j
+
+    if (Math.random() < 0.5) {
+        thickness = 4
+        inc = -inc
+    }
+
+    canvas.save()
+
+    canvas.translate(0.5 * cwidth, 0.5 * cheight)
+    canvas.rotate(Math.random() < 0.5? 0.5 * a: a)
+    canvas.translate(-0.5 * cwidth, -0.5 * cheight)
+
+    canvas.globalCompositeOperation = 'source-atop'
+
+    for (i = -pad; i < cwidth / step + pad; ++i) {
+        for (j = -pad; j < cheight / step + pad; ++j) {
+            canvas.beginPath()
+            canvas.arc((i + 0.5) * step, (j + 0.5) * step, thickness += inc, 0, 2 * Math.PI)
+            canvas.fill()
+        }
+    }
+
+    canvas.restore()
 }
 
 
