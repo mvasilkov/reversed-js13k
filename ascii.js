2c7c5eb3d514c858dbf690c1375b315b6badb0d5
Mark Vasilkov <mvasilkov@gmail.com>
2015-09-05 17:40:41 +0300

    computeCharacter()

--- ascii.js
+++ ascii.js
@@ -1,22 +1,20 @@
 var charAnimation = charMovingParts.map(function (a) {
-    if (a.indexOf('\n') == -1)
-        a += '\n            '
     return charStaticPart + a
 })
 
 var frame = 0
-var character = document.createElement('p')
+var $character = document.createElement('p')
 var charTextNode = document.createTextNode(charAnimation[frame])
 
-character.className = 'a' + frame
-character.appendChild(charTextNode)
-p.appendChild(character)
+$character.className = 'a' + frame
+$character.appendChild(charTextNode)
+$p.appendChild($character)
 
 
-var playerwidth = 82, playerheight = 103
+var playerwidth = 82, playerheight = 90
 
-asciizoomx = playerwidth / character.clientWidth
-asciizoomy = playerheight / character.clientHeight
+asciizoomx = playerwidth / $character.clientWidth
+asciizoomy = playerheight / $character.clientHeight
 asciileft = 0.5 * (cwidth - playerwidth)
 asciitop = 0.5 * (cheight - playerheight)
 handleResize()
@@ -25,6 +23,6 @@ handleResize()
 setInterval(function () {
     if (++frame == charAnimation.length)
         frame = 0
-    character.className = 'a' + frame
+    $character.className = 'a' + frame
     charTextNode.nodeValue = charAnimation[frame]
 }, 50)
---
+++ main.js
@@ -0,0 +1,75 @@
+var character = {
+    x: 10,
+    y: 0.5 * (cheight - playerheight),
+    width: playerwidth,
+    height: playerheight,
+    g: 0.1,
+    velocityx: 0.1,
+    velocityy: 0
+}
+
+var currentLevel = 0
+var platforms = levels[currentLevel].p
+var walls = levels[currentLevel].w
+
+var then
+
+function computeCharacter() {
+    var now = Date.now()
+    var t = now - then
+    then = now
+
+    character.velocityx = 4
+    character.velocityy += t * character.g
+
+    var position = [{x: character.x, y: character.y + character.height}]
+    position.push({x: position[0].x + character.width, y: position[0].y})
+
+    var expectation = [{
+        x: position[0].x + character.velocityx,
+        y: position[0].y + character.velocityy
+    }]
+    expectation.push({x: expectation[0].x + character.width, y: expectation[0].y})
+
+    expectation.velocityy = character.velocityy
+
+    platforms.forEach(function (platform) {
+        collide(position, expectation, platform)
+    })
+
+    character.y = expectation[0].y - character.height
+    character.velocityy = expectation.velocityy
+
+    /* magic */
+
+    position = [{x: character.y, y: character.x + character.width}]
+    position.push({x: position[0].x + character.height, y: position[0].y})
+
+    expectation = [{
+        x: position[0].x + character.velocityy,
+        y: position[0].y + character.velocityx
+    }]
+    expectation.push({x: expectation[0].x + character.height, y: expectation[0].y})
+
+    expectation.velocityy = character.velocityx
+
+    walls.forEach(function (platform) {
+        platform = [{x: platform[0].y, y: platform[0].x}, {x: platform[1].y, y: platform[1].x}]
+        collide(position, expectation, platform)
+    })
+
+    character.x = expectation[0].y - character.width
+    character.velocityx = expectation.velocityy
+}
+
+function mainloop() {
+    requestAnimationFrame(mainloop)
+    computeCharacter()
+
+    asciileft = character.x
+    asciitop = character.y
+    asciiUpdate()
+}
+
+then = Date.now()
+requestAnimationFrame(mainloop)
