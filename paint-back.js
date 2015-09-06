fe6f2d241a18e113dca32434df66c309fe996bdf
Andrey Shuster <andrey@tangentlogic.com>
2015-09-06 13:59:50 +0300

    speed change

--- paint-back.js
+++ paint-back.js
@@ -14,8 +14,8 @@ function drawStars(stars, offset) {
         s = stars[i].size;
     c.fillStyle = "rgba(255, 255, 255, "+stars[i].o+")";
     stars[i].off -= offset;
-    if (y < 0) {
-      stars[i].off = cheight;
+    if (y < cellsize) {
+      stars[i].off = cheight-cellsize;
       stars[i].x = rnd(0, cwidth);
     }
     c.fillRect(x, y, s, s);
@@ -25,7 +25,7 @@ function drawStars(stars, offset) {
 function prepareStars(opacity, size) {
   var stars = [];
   for (var i=0; i < 100; i++) {
-    stars.push({x:rnd(0, cwidth), y:rnd(0, cheight), o: opacity, off: 0, size: size});
+    stars.push({x:rnd(0, cwidth), y:rnd(cellsize, cheight-cellsize), o: opacity, off: 0, size: size});
   }
   return stars;
 }
@@ -35,8 +35,8 @@ stars2 = prepareStars(0.6, 2);
 stars3 = prepareStars(1, 3);
 
 function paintBackdrop(direction) {
-  drawStars(stars1, 2 * direction);
-  drawStars(stars2, 8 * direction);
-  drawStars(stars3, 15 * direction);
+  drawStars(stars1, 4 * direction);
+  drawStars(stars2, 16 * direction);
+  drawStars(stars3, 30 * direction);
 }
 
